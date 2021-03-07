a4abba1d91fba942fc914d708c979e842bc6fded
assimarakhimbekova <asima.astana@outlook.com>
2021-03-07 20:52:15 +0300

    added homework-lesson_6

--- lesson_6/task6_3.py
+++ lesson_6/task6_3.py
@@ -7,3 +7,19 @@ class Worker:
     (get_total_income). Проверить работу примера на реальных данных (создать экземпляры класса Position,
     передать данные, проверить значения атрибутов, вызвать методы экземпляров).
     '''
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+me =  Position('Assima', 'Rakh', 'Phd', 100_000, 50_000)
+print(me.get_full_name())
+print(me.get_total_income())
